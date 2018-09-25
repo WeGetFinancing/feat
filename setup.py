db71720b3bd96fa3fc7b541f1d19933d824b8ffb
Alberto Montañola <amontanola@getfinancing.com>
2018-09-25 16:14:18 +0200

    bumped version num

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 NAME = 'feat'
 
-VERSION = '18.30.1'
+VERSION = '18.38'
 DESCRIPTION = 'Flumotion Asynchronous Autonomous Agent Toolkit'
 LONG_DESC = DESCRIPTION
 AUTHOR = 'Flumotion Developers',
