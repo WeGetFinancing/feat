c2dd614f590e844dc19cc142618d42db1aa46bf9
Jenkins CI <jenkins@getfinancing.com>
2018-10-23 16:45:55 +0000

    Deployment of version 18.40.10.dev1, build 12 [ci skip]

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/GetFinancing/feat',
-    version='18.40.10',
+    version='18.40.10.dev1',
     zip_safe=False,
 )
 
