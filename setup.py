f53faec5da8932cc29de62ac8fa38305536671f3
Alberto <amontanola@getfinancing.com>
2018-12-18 12:48:47 +0100

    bump

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/GetFinancing/feat',
-    version='18.44.6.dev1',
+    version='18.50',
     zip_safe=False,
 )
 
